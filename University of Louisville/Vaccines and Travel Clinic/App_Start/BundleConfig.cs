5843a26c3659bb6f6993f8ca6fa7a2ee6eb11241
maseba01 <maseba01@louisville.edu>
2015-04-14 11:25:22 -0400

    jquery datatable fix

--- University of Louisville/Vaccines and Travel Clinic/App_Start/BundleConfig.cs
+++ University of Louisville/Vaccines and Travel Clinic/App_Start/BundleConfig.cs
@@ -13,6 +13,9 @@ namespace IdentitySample
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquerydata").Include(
+                "~/Scripts/DataTables/jquery.dataTables.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,7 +28,8 @@ namespace IdentitySample
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/carousel.css"));
+                      "~/Content/carousel.css",
+                      "~/Content/DataTables/css/jquery.dataTables.css"));
         }
     }
 }
